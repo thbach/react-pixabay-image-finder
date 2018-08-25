d0e2b605bf74175f6aadd7f3f92ee3bf127ed09d
tbach <thbach@gmail.com>
2018-08-25 12:21:00 -0700

    done

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NavBar from './components/Navbar/Navbar';
+import Search from './components/Search/Search';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <NavBar />
+          <Search />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
